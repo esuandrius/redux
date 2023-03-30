a6f7fcac8798775476a8a9785ec13bf6afeddc8d
esuandrius.github.io <111871226+esuandrius@users.noreply.github.com>
2023-03-30 13:38:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import usersReducer from "./features/Users";
 
+// Store
 const store = configureStore({
   reducer: {
     users: usersReducer,
